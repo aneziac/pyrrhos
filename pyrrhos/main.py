11df50dd5d0d32f3ab0679a0d95a2b30e2b2a85c
aneziac <nateannau@gmail.com>
2021-03-01 16:21:58 -0800

    site version 2

--- pyrrhos/main.py
+++ pyrrhos/main.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 
 class Page:
@@ -23,52 +24,83 @@ class Page:
         self.main_text = ""
 
     def cross_reference(self, other_page):
-        if self != other_page:
-            for word in other_page.vocab:
-                if word == 'Nardone':
-                    continue
-                mod_word = re.sub(r"\([^()]*\)", "", word).rstrip(' ()')
-                for v in [mod_word, mod_word.lower(), mod_word + 's', mod_word.lower() + 's']:
-                    for w in [' ' + v + ' ', ' ' + v + ',', ' ' + v + '.', ' ' + v + '!']:
-                        self.main_text = self.main_text.replace(w, w[0] + f'<a href="{other_page.url + ".html"}#{word}">{v}</a>' + w[-1])
+        if self == other_page:
+            return
+        for word in other_page.vocab:
+            clean_word = clean_parens(word)
+            if True in [clean_word in clean_parens(word2) for word2 in self.vocab]:
+                continue
+
+            for v in [clean_word, clean_word + 's', clean_word[:-2] + 'an', clean_word + 'ish', clean_word[-1] + 'ves']:
+                for w in [v, v.lower()]:
+                    for x in [' ' + w + ' ', ' ' + w + ',', ' ' + w + '.', ' ' + w + '!', ' ' + w + ')', ' ' + w + "’", ' ' + w + '?']:
+                        self.main_text = self.main_text.replace(x, x[0] + f'<a href="{other_page.url + ".html"}#{word}">{v}</a>' + x[-1])
 
     def write(self):
-        with open(self.url + '.html', 'w') as f:
-            # CSS
-            f.write('<link rel="stylesheet" href="style.css">\n')
+        with open('html/' + self.url + '.html', 'w') as f:
 
-            # Title
-            f.write('<h1>Pyrrhos</h1>\n')
-
-            # Links
-            f.writelines(['<h2>'] + [f'<a href="{url + ".html"}">{url.upper()}</a> ' if self.url != url else f'{url.upper()} ' for url in Page.urls] + ['</h2>\n'])
+            # Head
+            f.write('<!doctype html>\n<html>\n<head>\n')
+            f.write('<link rel="stylesheet" href="../css/header.css">\n<link rel="stylesheet" href="../css/body.css">\n')
+            f.write(f'<title>\n\tPyrrhos - {self.url.capitalize()}\n</title>\n')
 
             # Header
+            f.write('</head>\n<body>\n')
+            f.write('<h1>\n\tPyrrhos\n</h1>\n')
+            f.writelines(['<h2>\n\t'] + [f'<a href="{url + ".html"}">{url.upper()}</a> ' if self.url != url else f'{url.upper()} ' for url in Page.urls] + ['\n</h2>\n'])
             f.writelines(self.header_text)
 
             # Table of Contents
             if len(self.vocab) > 0:
                 f.write('<toc>\n<li><strong>CONTENTS</strong>\n</li>\n')
-                f.writelines([f'<li><blockquote>\n<a href="#{word}">{word}</a>\n</li></blockquote>\n' for word in self.vocab])
+                f.writelines([f'<li><blockquote>\n<a href="#{word}">{word}</a>\n</blockquote></li>\n' for word in self.vocab])
                 f.write('</toc>\n')
 
-            # Body
+            # Main text
             f.writelines(self.main_text)
 
+            f.write('</body>\n</html>')
+
+    def add_images(self, image_folder, img_class='character'):
+        file_path = './images/' + image_folder
+        images = [f for f in sorted(os.listdir(file_path)) if (f.endswith('.png') or f.endswith('.jpg'))]
+        descriptions = [f for f in os.listdir(file_path) if f.endswith('.txt')]
+        for source in images:
+            self.main_text += f'<img class="{img_class}" src=".{file_path}/{source}" alt="picture here">\n'
+            desc = os.path.splitext(source)[0] + '.txt'
+            if desc in descriptions:
+                with open(file_path + '/' + desc) as f:
+                    self.main_text += f'<cap>\n\t{f.readlines()[0]}</cap>\n'
 
 def download_source():
     import gdown
     import pypandoc
 
-    source = 'https://docs.google.com/document/export?format=docx&id=1ytATWxoHUMBGNWV_XeqcI-OS9cWWSE_pKVszt2ZTQuE'
+    source = 'https://docs.google.com/document/export?format=docx&id=10zOwNbnFIhr0NnuXhmXsRoRdr_eq7BBZ2lnI3Hb8Gw0'
     gdown.download(source, 'pyrrhos.docx', quiet=True)
     pypandoc.convert_file('pyrrhos.docx', 'html', outputfile="raw.html")
 
 
+def clean_term(term):
+    return term.replace(' -', '').replace('<u>', '').replace('</u>', '').rstrip()
+
+
+def clean_parens(term):
+    return re.sub(r"\([^()]*\)", "", term).rstrip(' ()')
+
+
 def build_website():
     pages, page_index = [], 1
-    for page_title in ['Home Page', 'A Geographical Overview of Pyrrhos', 'Political Overview of Pyrrhos', 'The Races of Pyrrhos', 'Religions', 'Monsters', 'Cosmology']:
+    for page_title in ['Home Page',
+                       'A Geographical Overview of Pyrrhos',
+                       'Political Overview of Pyrrhos',
+                       'The Races of Pyrrhos',
+                       'Religions',
+                       'Monsters',
+                       'Cosmology']:
         pages.append(Page(page_title))
+
+    pages[0].add_images('world', 'cover')
 
     with open('raw.html', 'r') as f:
         lines = f.readlines()
@@ -76,20 +108,20 @@ def build_website():
 
         header_feed = True
         for line in lines:
-            if header_feed and ('<strong' in line or '<ol' in line or '<em' in line):
+            if header_feed and True in [t in line for t in ['<strong', '<ol', '<em', 'ul']]:
                 header_feed = False
             if '<strong>' in line:
                 for title in Page.titles:
-                    if title in line:
+                    if title in line and ' -' not in line:
                         current_page = pages[page_index]
                         page_index += 1
                         header_feed = True
                 if current_page.url != 'home':
                     term = re.search("<strong>(.*)</strong>", line)
                     if term is not None:
-                        term = term.group(1).replace(' -', '').replace('<u>', '').replace('</u>', '').rstrip()
+                        term = clean_term(term.group(1))
                         current_page.main_text += f'<a name="{term}"></a>'
-                        if current_page.url not in ['monsters', 'cosmology'] and term not in Page.titles:
+                        if term not in Page.titles:
                             current_page.vocab.append(term)
 
             if header_feed:
@@ -98,14 +130,23 @@ def build_website():
                 current_page.main_text += line
 
     world_map = Page('Map of the World')
-    world_map.main_text += '<img src="./erebos.png" alt="Map of Erebos">\n<img src="./orestes.png" alt="Map of Orestes">'
-    pages.append(world_map)
+    world_map.add_images('map', 'map')
+    world_map.write()
+
+    players = Page('Players of Campaign')
+    players.add_images('players')
+    players.write()
+
+    npcs = Page('NPCs of the World')
+    npcs.add_images('npcs')
+    npcs.write()
+
+    pages = pages + [world_map, players, npcs]
 
     for page1 in pages:
         for page2 in pages:
             page1.cross_reference(page2)
         page1.write()
-
 
 def main():
     # download_source()
